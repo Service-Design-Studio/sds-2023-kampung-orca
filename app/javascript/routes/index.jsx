0d4ee19a7dac1ceed359c15bd30803f5801f1441
Kelly <kellycheongkahyen@gmail.com>
2023-06-20 22:06:58 +0800

    oops

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../components/Home";
+import LessonView from "../components/LessonView";
 
+export default (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LessonView />} />
+    </Routes>
+  </Router>
+);
