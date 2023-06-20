378dc542a1e3a931d0bfc50b48360d26378559d4
qazer25 <claysonsoh@hotmail.com>
2023-06-20 12:00:06 +0800

    added controllers, updated db

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,14 @@ Rails.application.routes.draw do
       get 'exercise/create'
       get 'exercise/show/:id', to: 'exercise#show'
       get 'exercise/destroy', to: 'exercise#destroy'
-
       get 'topic/index'
       get 'topic/create'
       get 'topic/show', to: 'topic#show'
-      get 'topic/destroy' to: 'topic#destroy'
-
+      get 'topic/destroy', to: 'topic#destroy'
       get 'page/index'
       post 'page/create'
       get 'page/show/:id', to: 'page#show'
       get 'page/destroy/:id', to: "page#destroy"
-
       get 'lesson/index'
       post 'lesson/create'
       get 'lesson/show/:id', to: 'lesson#show'
