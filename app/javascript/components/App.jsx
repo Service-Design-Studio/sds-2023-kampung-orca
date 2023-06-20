265291580c17231766b1f0492c7d81daf1821d81
pyraxo <pyraxo@pyraxo.moe>
2023-06-20 22:23:48 +0800

    Fix routing and rendering

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,29 +1,12 @@
-import React, { useState } from 'react';
-import LessonView from './LessonView';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export function App(props) {
-  const [lessonId, setLessonId] = useState(null);
+import Routes from "../routes";
 
-  const handleLessonClick = (id) => {
-    setLessonId(id);
-  };
-
-  const lessonIds = [123, 456, 789]; // Static array of lesson IDs
-
+export default App = (props) => {
   return (
-    <div className="app">
-      <h1>Lesson View</h1>
-      {lessonIds.map((id) => (
-        <button key={id} onClick={() => handleLessonClick(id)}>
-          Lesson {id}
-        </button>
-      ))}
-      {lessonId && (
-        <>
-          <p>Lesson duration: 11min</p>
-          <LessonView lessonId={lessonId} />
-        </>
-      )}
-    </div>
+    <ChakraProvider>
+      <>{Routes}</>
+    </ChakraProvider>
   );
-}
+};
--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-import { App } from './App.jsx'
-
-ReactDOM.createRoot( 
-  document.querySelector('#root')
-).render(<App />)
+document.addEventListener("turbo:load", () => {
+  const root = createRoot(
+    document.body.appendChild(document.createElement("div"))
+  );
+  root.render(<App />);
+});
