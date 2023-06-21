121a712a3e4cc39731ab1df061ece11dec03fa34
kiithy <thekeithchua@gmail.com>
2023-06-21 17:26:55 +0800

    Continue merge

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -10,17 +10,11 @@ import Chat from "../components/Chatbox/Chat";
 export default (
   <Router>
     <Routes>
-<<<<<<< HEAD
       <Route path="/lesson-view/:lesson_id" element={<LessonView />} />
       <Route path="/page" element={<Pages />} />
       <Route path="/lesson-complete/:lesson_id" element ={<LessonCompletion/>} />
       <Route path="/" element = {<LessonPathway/>} />
-=======
-      <Route path="/" element={<LessonView />} />
-      <Route path="/lesson-complete" element ={<LessonCompletion/>} />
-      <Route path="/lesson-pathway" element = {<LessonPathway/>} />
       <Route path="/chat" element={<Chat />} />
->>>>>>> refs/remotes/origin/lesson-complete
     </Routes>
   </Router>
 );
