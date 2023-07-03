adfbca7cc1f78fa762420f484f1064c5be301306
pyraxo <pyraxo@pyraxo.moe>
2023-07-03 18:45:20 +0800

    Deploy API gateway

--- api-gateway/config/application.rb
+++ api-gateway/config/application.rb
@@ -1,10 +1,20 @@
 require_relative "boot"
 
-require "rails/all"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+key_file = File.join "config", "master.key"
+if File.exist? key_file
+  ENV["RAILS_MASTER_KEY"] = File.read key_file
+end
 
 module ApiGateway
   class Application < Rails::Application
