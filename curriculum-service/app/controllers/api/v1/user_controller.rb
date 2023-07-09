5ef1ccc228f40dbfc9c44dd642add131c41b3b96
qazer2525 <1006005@mymail.sutd.edu.sg>
2023-07-09 21:32:29 +0800

    fixed some logic problems in creating user and added exercise in return json in completed lesson in curriculum

--- curriculum-service/app/controllers/api/v1/user_controller.rb
+++ curriculum-service/app/controllers/api/v1/user_controller.rb
@@ -3,7 +3,13 @@ class Api::V1::UserController < ApplicationController
 
 
   def create
-    user = User.create!(user_id: params[:user_id], lessons_access: [], exercises_access:[])
+
+    first_lessons_of_topic = Lesson.where(order_index: 0).to_a
+    lessons_array = []
+    first_lessons_of_topic.each do |lesson|
+      lessons_array << lesson.lesson_id
+    end
+    user = User.create!(user_id: params[:user_id], lessons_access: lessons_array, exercises_access:[])
     if user
       head 202
     else
@@ -19,21 +25,20 @@ class Api::V1::UserController < ApplicationController
     pre_lesson = Lesson.find_by(topic_id: topic_id, order_index: current_lesson.order_index - 1)
     user = User.find(params[:user_id])
     unless user.lessons_access.include?(next_lesson.lesson_id)
-      user.lessons_access << next_lesson.lesson_id  if next_lesson
+      user.lessons_access << next_lesson.lesson_id if next_lesson
     end
-
-
-    next_exercise = Exercise.find_by(params[next_lesson.lesson_id]) if next_lesson
-    unless user.exercises_access.include?(next_exercise.exercise_id)
-      user.exercises_access << next_exercise.exercise_id if next_exercise
+    
+    this_exercise = Exercise.find_by(params[current_lesson.lesson_id]) if current_lesson
+    unless user.exercises_access.include?(this_exercise.exercise_id)
+      user.exercises_access << this_exercise.exercise_id if this_exercise
     end
-
     user.save
 
   
     response = {
       next_lesson: next_lesson&.id,
-      pre_lesson: pre_lesson&.id
+      pre_lesson: pre_lesson&.id,
+      this_exercise: this_exercise&.id
     }
   
     render json: response
