367aba4c13a97718048f59a40e7a6c964715b437
pyraxo <pyraxo@pyraxo.moe>
2023-07-13 04:01:30 +0800

    [frontend] Remove empty component

--- frontend/src/components/Logins/CoverLogin.jsx
+++ frontend/src/components/Logins/CoverLogin.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Stack, Text, Button, Fade, ScaleFade } from "@chakra-ui/react";
-import DrawImageAnimation from "./DrawImageAnimation";
+import { Link } from "react-router-dom";
+import { Stack, Text, Button, ScaleFade } from "@chakra-ui/react";
 
 const CoverLogin = () => {
   const [showContent, setShowContent] = useState(false);
@@ -23,7 +22,6 @@ const CoverLogin = () => {
       fontFamily="Arial"
       position="relative" // Add position relative to contain the DrawImageAnimation component
     >
-      <DrawImageAnimation /> {/* Add the DrawImageAnimation component */}
       <ScaleFade initialScale={0.9} in={showContent} transitionDuration={1000}>
         <Text
           fontSize={["32px", "48px", "64px", "96px"]}
@@ -33,48 +31,56 @@ const CoverLogin = () => {
         >
           Welcome to Interfaith
         </Text>
+      </ScaleFade>
+      <Link to="/login">
+        <ScaleFade
+          initialScale={0.9}
+          in={showContent}
+          transitionDuration={2000}
+        >
+          <Button
+            fontSize={["24px", "32px", "48px"]}
+            fontWeight="700"
+            lineHeight="normal"
+            bg="#ed2e38"
+            textColor="#FFFFFF"
+            mt="4"
+            mb="4"
+            fontFamily="Arial"
+            size="lg"
+            height="56px"
+            shadow="md"
+            className="transition-all duration-300 transform-gpu hover:scale-110"
+          >
+            Login
+          </Button>
         </ScaleFade>
-        <Link to="/login">
-          <ScaleFade initialScale={0.9} in={showContent} transitionDuration={2000}>
-            <Button
-              fontSize={["24px", "32px", "48px"]}
-              fontWeight="700"
-              lineHeight="normal"
-              bg="#ed2e38"
-              textColor="#FFFFFF"
-              mt="4"
-              mb="4"
-              fontFamily="Arial"
-              size="lg"
-              height="56px"
-              shadow="md"
-              className="transition-all duration-300 transform-gpu hover:scale-110"
-            >
-              Login
-            </Button>
-          </ScaleFade>
-        </Link>
-        <Link to="/learnmore">
-          <ScaleFade initialScale={0.9} in={showContent} transitionDuration={3000}>
-            <Button
-              fontSize={["24px", "32px", "48px"]}
-              fontWeight="700"
-              lineHeight="normal"
-              bg="#ed2e38"
-              textColor="#FFFFFF"
-              mt="4"
-              mb="4"
-              fontFamily="Arial"
-              size="lg"
-              height="56px"
-              shadow="md"
-              className="transition-all duration-300 transform-gpu hover:scale-110"
-            >
-              Learn more
-            </Button>
-          </ScaleFade>
-        </Link>
-      </Stack>
+      </Link>
+      <Link to="/learnmore">
+        <ScaleFade
+          initialScale={0.9}
+          in={showContent}
+          transitionDuration={3000}
+        >
+          <Button
+            fontSize={["24px", "32px", "48px"]}
+            fontWeight="700"
+            lineHeight="normal"
+            bg="#ed2e38"
+            textColor="#FFFFFF"
+            mt="4"
+            mb="4"
+            fontFamily="Arial"
+            size="lg"
+            height="56px"
+            shadow="md"
+            className="transition-all duration-300 transform-gpu hover:scale-110"
+          >
+            Learn more
+          </Button>
+        </ScaleFade>
+      </Link>
+    </Stack>
   );
 };
 
