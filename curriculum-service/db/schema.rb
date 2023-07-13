b9e89f2d1c853b6c049aef2d707427e768d52977
pyraxo <pyraxo@pyraxo.moe>
2023-07-13 12:19:35 +0800

    [curriculum] Update schema

--- curriculum-service/db/schema.rb
+++ curriculum-service/db/schema.rb
@@ -12,40 +12,45 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_12_174931) do
   create_table "exercises", id: false, force: :cascade do |t|
-    t.integer "exercise_id"
+    t.integer "exercise_id", null: false
     t.integer "topic_id"
     t.integer "lesson_id"
     t.string "title"
     t.string "qns"
+    t.index ["exercise_id"], name: "index_exercises_on_exercise_id", unique: true
   end
 
   create_table "lessons", id: false, force: :cascade do |t|
-    t.integer "lesson_id"
+    t.integer "lesson_id", null: false
     t.integer "topic_id"
     t.string "title"
     t.integer "order_index"
     t.string "message"
+    t.index ["lesson_id"], name: "index_lessons_on_lesson_id", unique: true
   end
 
   create_table "pages", id: false, force: :cascade do |t|
-    t.integer "page_id"
+    t.integer "page_id", null: false
     t.integer "lesson_id"
     t.integer "order_index"
     t.string "video"
     t.string "words"
     t.json "sections"
+    t.index ["page_id"], name: "index_pages_on_page_id", unique: true
   end
 
   create_table "topics", id: false, force: :cascade do |t|
-    t.integer "topic_id"
+    t.integer "topic_id", null: false
     t.string "title"
     t.integer "num_of_lessons"
+    t.index ["topic_id"], name: "index_topics_on_topic_id", unique: true
   end
 
   create_table "users", id: false, force: :cascade do |t|
-    t.string "user_id"
+    t.string "user_id", null: false
     t.string "lessons_access"
     t.string "exercises_access"
+    t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
 end
