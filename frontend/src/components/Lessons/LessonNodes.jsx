6abe94f46c3289311720d722ed8231f86ea36679
kiithy <thekeithchua@gmail.com>
2023-07-17 17:12:22 +0800

    Added Disabled lesson functionality, rm progress

--- frontend/src/components/Lessons/LessonNodes.jsx
+++ frontend/src/components/Lessons/LessonNodes.jsx
@@ -20,62 +20,25 @@ import {
 import { Header } from "../Header";
 import useGateway from "../../hooks/useGateway";
 
-const nodesData = [
-  //test data for the dynamic nodes. will be taken out once backend is successfully linked
-  {
-    icon: BsCheckCircle,
-    score: "7/10",
-    progress: "100",
-    title: "Introduction to Interfaith",
-    message:
-      "Explore the rich tapestry of diverse religious traditions and foster a deeper understanding of interfaith dialogue and cooperation. Discover the commonalities and unique aspects of various faiths, promoting unity and respect.",
-  },
-  {
-    icon: BsEmojiSmile,
-    score: "10/10",
-    progress: "100",
-    title: "Interfaith Ethics and Values",
-    message:
-      "Delve into the ethical principles and moral values shared across different religious traditions. Learn how these teachings can guide individuals and communities towards compassionate action, social justice, and interfaith harmony.",
-  },
-  {
-    icon: BsStopCircle,
-    progress: "64",
-    title: "Interfaith Dialogue Techniques",
-    message:
-      "Learn effective communication strategies and dialogue techniques for engaging in meaningful interfaith conversations. Develop skills to foster mutual understanding, empathy, and constructive discussions among people of different faiths.",
-  },
-  {
-    icon: BsCircle,
-    progress: "0",
-    title: "Interfaith Festivals and Celebrations",
-    message:
-      "Discover the vibrant tapestry of interfaith festivals and celebrations from around the world. Explore the cultural expressions, rituals, and symbolism that promote interfaith unity, inclusivity, and shared joy.",
-  },
-  {
-    icon: BsCircle,
-    progress: "0",
-    title: "Interfaith Peacebuilding and Reconciliation",
-    message:
-      "Examine the role of interfaith efforts in promoting peace, conflict resolution, and reconciliation. Learn about inspiring examples of interfaith initiatives fostering harmony in diverse and divided societies.",
-  },
-];
+
 
 const DynamicNodes = () => {
   const [data] = useGateway(window.location.pathname + "/lesson", "GET");
-  if (!data) return;
+  console.log(data);
+  if (!data) return null;
+
+  const lessonsAccess = data.lessons_access;
+
   return data.lessons.map((node, index) => (
     <React.Fragment key={index}>
       {index !== 0 && <Line />}{" "}
       {/* Render the Line component only if index is not 0 */}
       <Link to={`/curriculum/lesson/${node.lesson_id}`}>
         <Node
-          //icon={node.icon}
-          //score={node.score}
-          progress="70"
-          //status={node.status}
           title={node.title}
           message={node.message}
+          lessonId={node.lesson_id}
+          lessonsAccess={lessonsAccess}
         />
       </Link>
     </React.Fragment>
@@ -103,74 +66,56 @@ const Node = ({
   color,
   title,
   message,
-  progress,
-  score,
+  lessonId,
+  lessonsAccess
 }) => {
   const [isHovered, setIsHovered] = React.useState(false);
 
+
+
+  const isEnabled = lessonsAccess.find((lesson) => lesson.lesson_id === lessonId);
   let status;
-  if (progress === "0") {
-    status = "Not Started";
-  } else if (progress === "100") {
-    status = "Complete";
-  } else {
-    status = "In Progress";
+  let statusColour;
+  let cursorStyle
+  if (isEnabled) {
+    status = "Unlocked";
+    statusColour = "green";
+    cursorStyle = "pointer";
+  } else{
+    status = "Locked";
+    statusColour = "red";
+    cursorStyle = "not-allowed";
   }
+
 
   return (
     <Popover trigger="hover" placement="top">
       <PopoverTrigger>
         <Stack
-          width="fit-content"
-          height="fit-content"
-          direction="column"
-          justify="center"
-          align="flex-end"
-          bg="white"
-          borderRadius="50%"
-          shadow="lg"
-          _hover={{
-            transform: "scale(1.05)",
-            boxShadow: "0 0 10px 5px rgba(0, 0, 0, 0.3)",
-          }}
+          opacity={isEnabled ? 1 : 0.5}
+          cursor={cursorStyle}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
           <Icon as={IconComponent} color={color} boxSize="80px" />
         </Stack>
       </PopoverTrigger>
-      <PopoverContent width="220px" boxShadow="0 0 5px 2px rgba(0, 0, 0, 0.3)">
+      <PopoverContent>
         <PopoverArrow />
         <PopoverHeader fontWeight="semibold">{title}</PopoverHeader>
         <PopoverBody fontSize="14px" textAlign="justify">
           {message}
         </PopoverBody>
-
-        <PopoverBody
-          fontSize="14px"
-          fontWeight="semibold"
-          color="green"
-          textAlign="justify"
-        >
+        <PopoverBody fontSize="14px" fontWeight="semibold" color={statusColour} textAlign="justify">
           <Stack direction="row" justifyContent="space-between">
             <Text>{status}</Text>
-            <Text>{score}</Text>
           </Stack>
-        </PopoverBody>
-        <PopoverBody>
-          <Progress
-            hasStripe
-            value={progress}
-            colorScheme="green"
-            transition="all 0.3s ease-in-out"
-            borderRadius="md"
-            height="8px"
-          />
         </PopoverBody>
       </PopoverContent>
     </Popover>
   );
 };
+
 
 export const LessonNodes = ({ lessonProgress }) => {
   return (
