3c468e9b38ef187cc276f3791ef923cc2ccb327e
matteolee72 <matthias.lee.dong.en@gmail.com>
2023-07-22 11:33:40 +0800

    finished topic:model rspec test

--- curriculum-service/app/models/topic.rb
+++ curriculum-service/app/models/topic.rb
@@ -1,7 +1,7 @@
 # app/models/topic.rb
 class Topic < ApplicationRecord
   has_many :lessons, dependent: :destroy
-  has_many :exercises
+  has_many :exercises, dependent: :destroy
   # has_many :exercise_lists
   # TODO: ID should be a string not integer!!
   self.primary_key = :topic_id
--- curriculum-service/spec/models/page_spec.rb
+++ curriculum-service/spec/models/page_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Page, type: :model do
   describe 'validations' do
-    it 'is valid with valid attributes' do
-      page = Page.new(page_id: '00001')
-      expect(topic).to be_valid
-    end
-
     it 'is not valid without a topic_id' do
       topic = Topic.new(title: "help")
       expect(topic).not_to be_valid
@@ -15,6 +10,25 @@ RSpec.describe Page, type: :model do
     it 'is valid without title' do
       topic = Topic.new(topic_id: '00001')
       expect(topic).to be_valid
+    end
+    it 'is valid with valid attributes' do
+      topic = Topic.create(topic_id: '00001')
+      lesson = Lesson.create(
+        lesson_id: "00001",
+        topic_id: topic.id
+      )
+
+      page = Page.new(page_id: "00001",
+      lesson_id: lesson.id,
+      order_index: 0,
+      video: "https://www.youtube.com/embed/QWTv8NbItt0",
+      sections: [
+        { content:["This is page 1 of lesson 1."]},
+        { title: "The Importance of Interfaith Dialogue", content: ["In today's ."]},
+        { title: "Exploring Religious Traditions", content: ["Interfaith."]},
+        { title: "Building Bridges of Understanding", content: ["In an increasingly."]}
+      ])
+      expect(page).to be_valid
     end
   end
 end
