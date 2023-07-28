e0800d325289fe31ea46d802dcea7db4fea3aa59
kiithy <thekeithchua@gmail.com>
2023-07-28 14:27:59 +0800

    Exercise WIP, pushed to start RESTful curriculum

---
+++ frontend/src/components/Lessons/Exercise.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import { Link, useParams, useHistory } from "react-router-dom";
+
+// export const Exercise = () => {
+//   const params = useParams();
+//   const [data] = useGateway(window.location.pathname, "Get");
+//   if (!data) return;
+
+//   const back_to_lesson_complete = `/curriculum/lesson/${params["lesson_id"]}/lesson_completed`;
+//   const lesson_id = data?.lesson_id;
+
