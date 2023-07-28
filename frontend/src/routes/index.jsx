daeffe4b3ef7fe5c9f5571fdeb2c1bc15d5c6728
kiithy <thekeithchua@gmail.com>
2023-07-28 17:50:10 +0800

    Set up exercise routing and frontend

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -11,6 +11,8 @@ import {
   LessonNodes,
   LessonView,
   LessonCompletion,
+  LessonSection,
+  Exercise,
 } from "../components/Lessons";
 
 import {
@@ -48,6 +50,10 @@ export default (
         <Route
           path="/curriculum/lesson/:lesson_id/error"
           element={<CurriculumErrorPage />}
+        />
+        <Route
+          path="/curriculum/lesson/:lesson_id/exercise"
+          element={<Exercise />}
         />
       </Route>
 
