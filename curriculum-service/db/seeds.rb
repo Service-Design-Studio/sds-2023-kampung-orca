48cc7d2a4f47f78cd89ba9be281adf0e0c5dcda4
kiithy <thekeithchua@gmail.com>
2023-07-31 19:03:03 +0800

    Added first page for 2nd topic first lesson

--- curriculum-service/db/seeds.rb
+++ curriculum-service/db/seeds.rb
@@ -122,4 +122,16 @@ Page.create!(
     { title: "Interfaith Dialogue: A Path to Global Peace", content: ["Interfaith dialogue holds the promise of contributing to global peace by addressing the root causes of conflict and division. By fostering understanding, empathy, and mutual respect, individuals from diverse religious backgrounds can transcend religious differences and work together toward shared goals. The principles of interfaith dialogue, such as inclusivity, justice, and cooperation, provide a framework for resolving conflicts and promoting social harmony. Through interfaith dialogue, we can envision a future where people of all faiths collaborate to create a more peaceful and just world for all."]}
   ]
 )
+
+Page.create!(
+  page_id: "00008",
+  lesson_id: "00006",
+  order_index: 0,
+  video: "https://www.youtube.com/embed/P_uKbiHBUHo",
+  sections: [
+    { content: ["This is page 1 of lesson 6."]},
+    { title: "Intermediate Interfaith Lessons", content: ["Explore the rich tapestry of diverse religious traditions and foster a deeper understanding of interfaith dialogue and cooperation. Discover the commonalities and unique aspects of various faiths, promoting unity and respect."]}
+  ]
+)
 p "Created #{Page.count} pages"
+
