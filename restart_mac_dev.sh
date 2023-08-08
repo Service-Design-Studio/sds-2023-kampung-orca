638149736ba91a68cd74e34e5386c8b5a4d6fb90
kiithy <thekeithchua@gmail.com>
2023-08-08 15:17:33 +0800

    Updated mac restart script

--- restart_mac_dev.sh
+++ restart_mac_dev.sh
@@ -18,6 +18,6 @@ osascript -e 'tell application "System Events"
     do script "cd '$PWD'/frontend && npm i" in selected tab of front window
     do script "cd .." in selected tab of front window
     do script "cd '$PWD'/inactivity-checker-service" in selected tab of front window
-    do script "bundle install --without production && rake db:drop && rake db:migrate && rake db:seed" in selected tab of front window
+    do script "bundle install --without production" in selected tab of front window
   end tell
 end tell'
